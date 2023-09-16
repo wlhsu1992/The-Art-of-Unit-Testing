de256c0686ca0cbbed25bbf42142d61c22839715
willhsu <wlhsu1992@hotmail.com>
2023-09-16 14:41:18 +0800

    ch2: 加入MemCalculator 測試案例

---
+++ TheArtOfUnitTesting/LogAn.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using Ch2_SimpleUnitTest;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class MemCalculatorTests
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnZero()
+        {
+            var calculator = MakeCalc();
+            var result = calculator.Sum();
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Add_AddOneThenAddTwo_ReturnOne()
+        {
+            var calculator = MakeCalc();
+            calculator.Add(1);
+            calculator.Add(2);
+
+            var result = calculator.Sum();
+            Assert.AreEqual(3, result);
+        }
+
+        private static MemCalaulator MakeCalc()
+        {
+            return new MemCalaulator();
+        }
+
+    }
+}
