0189e88457d8a65c8177227bc780c4b002922465
willhsu <wlhsu1992@hotmail.com>
2023-09-16 14:32:03 +0800

    ch2: 新增目標測試方法，相加及返回加總

---
+++ TheArtOfUnitTesting/Ch2_SimpleUnitTest/MemCalaulator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch2_SimpleUnitTest
+{
+    public class MemCalaulator
+    {
+        private int sum = 0;
+
+        /// <summary>
+        /// 將輸入值與當前加總進行相加
+        /// </summary>
+        /// <param name="number"></param>
+        public void Add(int number)
+        {
+            sum += number;
+        }
+
+        /// <summary>
+        /// 返回當前加總並將加總值歸 0
+        /// </summary>
+        /// <returns></returns>
+        public int Sum() 
+        {
+            int temp = sum;
+            sum = 0;
+            return temp;
+        }    
+    }
+}
